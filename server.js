0f80222481635708939e1b38bc8af4e69be8eb03
Jon Young <jyoung@dyn.com>
2018-03-15 09:24:51 -0400

    init commit

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+//mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useMongoClient: true,
+});
+
 mongoose.Promise = global.Promise;
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
@@ -18,9 +22,9 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
 }));
 
 app.use(passport.initialize());
@@ -30,5 +34,5 @@ routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+  console.log('Node.js listening on port ' + port + '...');
 });
